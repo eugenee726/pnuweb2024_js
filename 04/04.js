6a165c1dfd5e8fa0b70aa7b4396d68bb14b0c152
eugenee <jyp0726@pusan.ac.kr>
2024-07-01 22:25:22 +0900

    6 / 28

--- 04/04.js
+++ 04/04.js
@@ -1,38 +1,35 @@
-//사용자와 컴퓨터 랜덤수 생성
+//사용자와 컴퓨터의 랜덤수 생성
+//1. DOM 생성이 되면
+document.addEventListener('DOMContentLoaded', () => {
+  //img, button 요소를 가져오기
+  const imgCom = document.querySelector('#com');
+  const imgUser = document.querySelector('#user');
+  //queryselectAll이기 때문에 다 가져옴
+  const bts = document.querySelectorAll('#divContent button')
 
-// 1. DOM 생성이 되면 
-document.addEventListener('DOMContentLoaded', ()=>{
-    //img, button 요소를 가져오기
-    const imgCom = document.querySelector('#com')
-    const imgUser = document.querySelector('#user')
-    const bts = document.querySelectorAll('#divContent button')  //6개의 버튼을 한번에
-    const msg = document.querySelector('#msg') ;
-  
-    console.log(bts);
-  
-    // 2. button 클릭이 되는 경우를 찾아야 함
-    for(let bt of bts) {
-      bt.addEventListener('click', ()=>{
-        //랜덤수를 발생 1 ~ 6까지
-        const nCom = Math.floor(Math.random() * 6) + 1 ; 
-    
-        //user 숫자 :문자를 숫자로 변환
-        const nUser = parseInt(bt.textContent.charAt(0)); //parseInt 는 문자 정수변환
-        console.log('nUser', nUser) 
-    
-        // 3. img src 속성을 변경 
-        imgCom.setAttribute('src', `./img/${nCom}.png`) ; //기존 value 변경
-        imgUser.setAttribute('src', `./img/${nUser}.png`) ; //${} 괄호 안에 내용 출력
-  
-        //4. 숫자비교
-        if (nCom === nUser) {
-          msg.innerHTML = '<h1 id="msgRed">맞음</h1>';
+  const msg = document.querySelector('#msg');
+  // 2. button 클릭이 되는 경우를 찾아야 함
+  for (let bt of bts) {
+    bt.addEventListener('click', () => {
+      //랜덤수를 발생 1 ~ 6까지
+      const nCom = Math.floor(Math.random() * 6) + 1;
+      //console.log(`${nCom}.png`) ;
 
-        }
-        else {
-          msg.innerHTML = '<h1 id="msgBlue">틀림</h1>'
-        }
-      }) ;
-    }
-  
-  });
+      //user 숫자: 문자를 숫자로 변환
+      const nUser = parseInt(bt.textContent.charAt(0));
+      console.log('nUser', nUser);
+      // 3. img src 속성을 변경 
+      imgCom.setAttribute('src', `./img/${nCom}.png`);
+      imgUser.setAttribute('src', `./img/${nUser}.png`);
+
+      //4. 숫자비교
+      if(nUser === nCom) {
+        msg.innerHTML = '<h1>맞음</h1>';
+      }else{
+        msg.innerHTML = '<h1>틀림</h1>'
+      }
+    });
+  }
+
+
+});
